fcc87e502738da0ca7f4ae025911f8024aa264aa
khush <tailork.2008@gmail.com>
2026-05-13 17:10:17 -0400

    work

--- src/main/java/org/khush/User.java
+++ src/main/java/org/khush/User.java
@@ -5,6 +5,13 @@ import java.util.List;
 public abstract class User {
     protected String id;
     protected String name;
-    protected List<> bprrpwedItems;
+    protected List<> borrowedItems;
 
+    public static int nextId = 0;
+
+    public User(String name, List borrowedItems) {
+        this.id = id;
+        this.name = name;
+        this.borrowedItems = borrowedItems;
+    }
 }
