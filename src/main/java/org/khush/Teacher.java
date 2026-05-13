c81e424aaf1d418a33ee3d61179e70703770841c
khush <tailork.2008@gmail.com>
2026-05-13 17:27:25 -0400

    work

--- src/main/java/org/khush/Teacher.java
+++ src/main/java/org/khush/Teacher.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Teacher extends User{
 
-    public Teacher(String name, List borrowedItems) {
-        super(name, borrowedItems);
+    public Teacher(String name) {
+        super(name);
     }
 }
