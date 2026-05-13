d193feeae832e15fe6f4db0c4081dd50ea9645eb
khush <tailork.2008@gmail.com>
2026-05-13 17:20:21 -0400

    work

---
+++ src/main/java/org/khush/Admin.java
@@ -0,0 +1,10 @@
+package org.khush;
+
+import java.util.List;
+
+public class Admin extends User{
+
+    public Admin(String name, List borrowedItems) {
+        super(name, borrowedItems);
+    }
+}
