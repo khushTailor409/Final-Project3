097e6256dea36d875e82bdb6b7b635dbb6c7bdaf
khush <tailork.2008@gmail.com>
2026-05-13 17:21:03 -0400

    work

---
+++ src/main/java/org/khush/Student.java
@@ -0,0 +1,10 @@
+package org.khush;
+
+import java.util.List;
+
+public class Student extends User{
+
+    public Student(String name, List borrowedItems) {
+        super(name, borrowedItems);
+    }
+}
