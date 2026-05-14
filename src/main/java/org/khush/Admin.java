53d1e3b2f24536cb0ff5c9aec367749eb229fd51
khush <tailork.2008@gmail.com>
2026-05-13 20:03:25 -0400

    work

--- src/main/java/org/khush/Admin.java
+++ src/main/java/org/khush/Admin.java
@@ -1,8 +1,14 @@
 package org.khush;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.util.List;
 
-public class Admin extends User{
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+
+public class Admin extends User {
 
     public Admin(String name) {
         super(name);
